02e5eb9be8c50a2b92567ffb12d149b8fd220dde
mbehboodi <mbehboodi6909@gmail.com>
2018-07-15 16:01:38 +0430

    add `setup.py` for pypi packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="swagger-ui-updater",
+    version="1.0",
+    author="xeptore",
+    author_email="mbehboodi6909@.com",
+    description="Swagger UI updater works with node projects",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
